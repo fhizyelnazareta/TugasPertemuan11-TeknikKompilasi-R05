f77369bfb8bf75c6f6c3233a0b672c80ed703028
Fhizyel Nazareta <fhizyelblawers@gmail.com>
2020-12-23 09:30:37 +0700

    First Commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Tugas Pertemuan 11 Teknik Kompilasi R.05
+
+100 line code program dan code over looping pada bahasa pemrograman PHP
+
+# Author 
+# 1. Fhizyel Nazareta 183112706450116
+# 2. Sapto Wibowo 183112706450118
+
---
+++ over-looping.php
@@ -0,0 +1,9 @@
+<?php
+
+echo "Fhizyel Nazareta 183112706450116";
+echo "Sapto Wibowo 183112706450118";
+echo "\t=========== Program Infinite Loop =========\n\n";
+
+for ($i = 10; $i >= 0; $i++) {
+    echo "1. Looping Forever\n";
+}
